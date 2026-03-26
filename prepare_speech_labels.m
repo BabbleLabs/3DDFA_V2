0e909464443a5e59d30bf8ae3edb15353e98a45f
Michal Sulewski <msulewsk@cisco.com>
2026-03-26 14:23:24 +0000

    new video examples added

--- prepare_speech_labels.m
+++ prepare_speech_labels.m
@@ -11,10 +11,10 @@ clear; clc; close all;
 % singleTalk = [0.85 12.44; 13.82 23.34; 24.58 37.29; 38.36 49.38; 50.30 52.61];
 % doubleTalk = [];
 
-audioPath = 'inputs/';
-audioName = 'example_7.wav';
-singleTalk = [0.72 3.32; 11.32 15.19; 36.27 38.98; 40.99 44.17];
-doubleTalk = [3.36 11.19; 15.57 36.16; 39.14 40.99; 44.33 53.81];
+% audioPath = 'inputs/';
+% audioName = 'example_7.wav';
+% singleTalk = [0.72 3.32; 11.32 15.19; 36.27 38.98; 40.99 44.17];
+% doubleTalk = [3.36 11.19; 15.57 36.16; 39.14 40.99; 44.33 53.81];
 
 % audioPath = 'inputs/';
 % audioName = 'example_9.wav';
@@ -35,6 +35,66 @@ doubleTalk = [3.36 11.19; 15.57 36.16; 39.14 40.99; 44.33 53.81];
 % audioName = 'example_12.wav';
 % singleTalk = [1.28 18.02];
 % doubleTalk = [];
+
+% audioPath = 'inputs/';
+% audioName = 'example_13.wav';
+% singleTalk = [0.30 6.27; 19.40 27.43];
+% doubleTalk = [6.84 19.2];
+
+% audioPath = 'inputs/';
+% audioName = 'example_14.wav';
+% singleTalk = [0.30 6.88; 21.78 27.23];
+% doubleTalk = [6.92 21.37];
+
+% audioPath = 'inputs/';
+% audioName = 'example_15.wav';
+% singleTalk = [1.13 8.82; 28.85 37.92];
+% doubleTalk = [8.86 28.21];
+
+% audioPath = 'inputs/';
+% audioName = 'example_16.wav';
+% singleTalk = [0.57 9.20; 29.20 39.32];
+% doubleTalk = [9.76 28.93];
+
+% audioPath = 'inputs/';
+% audioName = 'example_17.wav';
+% singleTalk = [0.35 5.85; 18.13 25.46];
+% doubleTalk = [7.01 17.91];
+
+% audioPath = 'inputs/';
+% audioName = 'example_18.wav';
+% singleTalk = [0.75 6.98; 18.89 27.12];
+% doubleTalk = [7.02 18.49];
+
+% audioPath = 'inputs/';
+% audioName = 'example_19.wav';
+% singleTalk = [14.28 27.25; 41.38 46.25];
+% doubleTalk = [0.80 13.37; 27.84 39.51];
+
+% audioPath = 'inputs/';
+% audioName = 'example_20.wav';
+% singleTalk = [10.93 20.73; 33.77 39.94];
+% doubleTalk = [1.08 10.20; 22.50 32.26];
+
+% audioPath = 'inputs/';
+% audioName = 'example_21.wav';
+% singleTalk = [10.99 18.28; 32.07 41.20];
+% doubleTalk = [1.30 10.33; 20.12 30.44];
+
+% audioPath = 'inputs/';
+% audioName = 'example_22.wav';
+% singleTalk = [11.99 23.13; 36.65 40.41];
+% doubleTalk = [0.82 9.39; 25.00 35.82];
+
+% audioPath = 'inputs/';
+% audioName = 'example_23.wav';
+% singleTalk = [7.25 22.5; 32.23 49.35];
+% doubleTalk = [0.36 6.57; 23.47 32.08];
+
+audioPath = 'inputs/';
+audioName = 'example_24.wav';
+singleTalk = [7.32 21.82; 32.77 48.34];
+doubleTalk = [0.55 6.07; 23.16 32.25];
 
 %% Processing
 fsTarget = 48e3;
